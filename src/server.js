43757b9aa864aea92583c21df25836f5eb9c0cc6
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-08-16 18:54:56 -0300

    partials

--- src/server.js
+++ src/server.js
@@ -12,6 +12,6 @@ server
     return res.render('index.html', { cards })
 })
 .get('/oms', (req, res) => {
-    return res.render('partials/oms.html')
+    return res.render('partials/oms.html', { title:'OMS', descriptionTitle: 'Total conectividade e flexibilidade em toda operação', description: 'Promove uma visão e uma jornada de compra unificada para cada cliente, de forma que ele tenha uma melhor experiência, seja qual for o canal de entrada, além da visibilidade integrada dos seus estoques.'})
 })
 .listen(5500)
