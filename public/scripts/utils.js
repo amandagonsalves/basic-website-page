21be409da20c4ccfaf4563f7be7549f2c8ab356b
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-08-25 14:41:29 -0300

    select

--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -5,3 +5,32 @@ document.querySelector('.icon-menu').addEventListener('click', e => {
 document.querySelector('.icon-close').addEventListener('click', e => {
     menu.classList.remove('show')
 })
+function populateUfs() {
+    const ufSelect = document.querySelector('#state');
+    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+    .then(res => res.json())
+    .then(states => {
+        for (let state of states) {
+            ufSelect.innerHTML += ` <option value="${state.id}">${state.nome}</option>`
+        }
+    })
+}
+populateUfs()
+function getCities(event) {
+    const citySelect = document.querySelector('#city');
+    const ufValue = event.target.value;
+    const stateInput = document.querySelector('input#state')
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text;
+    citySelect.innerHTML = '';
+    citySelect.disabled = true;
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+    fetch(url)
+    .then(res => res.json())
+    .then(cities => {
+        for (let city of cities) {
+            citySelect.innerHTML += ` <option value="${city.id}">${city.nome}</option>`
+        }
+    })
+}
+document.querySelector('#state').addEventListener('change', getCities)
