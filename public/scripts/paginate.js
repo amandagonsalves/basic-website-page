52c9d61bfa9e89731b5174018d3b168a31fe5dd9
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-08-31 17:08:49 -0300

    paginate

--- public/scripts/paginate.js
+++ public/scripts/paginate.js
@@ -1,17 +1,27 @@
+const fetch = require('node-fetch');
 function populateCards() {
-    console.log(cards)
+    const data = fetch('https://gist.githubusercontent.com/amandagonsalves2/2c33f0c52b7b666081c206fba6d55807/raw/2c2f9fc12c371ffe2597b98307942bb245f18de8/test-json')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.length)
+            const state = {
+                page: 1,
+                perPage: 6,
+                totalPages: Math.ceil(data.length / 6)
+            }
+            console.log(state.totalPages)
+        });
+    const controls = {
+        next() {
+            state.page++;
+            const lastPage = state.page > state.totalPages
+            if(lastPage) {
+                state.page--
+            }
+            console.log(state.totalPages)
+        },
+        prev() { },
+        goTo() { }
+    }
 }
-populateCards();
-const state = {
-    page: 1,
-    perPage: 6,
-    totalPages: cards.length/6
-}
-const controls = {
-    next() {
-        state.page++
-        console.log(state.totalPages)
-    },
-    prev() {},
-    goTo() {}
-}
+populateCards()
