c095f7694bf7a53efc7d3a379a0c4a1443a09260
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-09-02 14:34:10 -0300

    paginate

--- src/paginate.js
+++ src/paginate.js
@@ -1,7 +1,26 @@
 function paginate(data) {
     console.log('ola,');
     console.log(data)
-    
+
+    let perPage = 6;
+    const state = {
+        page: 1,
+        perPage,
+        totalPage: Math.ceil(data.length/perPage)
+    };
+    const controls = {
+        next() {
+            state.page++;
+
+            const lastPage = state.page > state.totalPage;
+            if(lastPage) {
+                state.page--;
+            }
+        },
+        prev() {},
+        goTo() {}
+    }
+    console.log(state.page)
 }
 module.exports =  {
     paginate
