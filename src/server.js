d8b19e0b40beaa4cfa3dbeec6051c97250914c72
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-09-14 22:31:07 -0300

    json

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const Bundler = require('parcel-bundler');
-const server = require('express')();
+const express = require('express');
+const server = express();
 const { join } = require('path');
 
 server.set('view engine', 'ejs');
@@ -9,5 +10,7 @@ const options = {};
 const bundler = new Bundler([join(__dirname, './views/index.pug'), join(__dirname, './views/contact.pug'), join(__dirname, './views/blog.pug'), join(__dirname, './views/work-with-us.pug'), join(__dirname, './views/partials/applicant-email.pug'), join(__dirname, './views/partials/emites.pug'), join(__dirname, './views/partials/myFinance.pug'),join(__dirname, './views/partials/oms.pug'), join(__dirname, './views/partials/pdvend.pug'), join(__dirname, './views/partials/store.pug'), join(__dirname, './views/partials/suite.pug')], options);
 
 server.use(bundler.middleware());
+
+server.use(express.urlencoded({ extended: true }))
 
 server.listen(5500);
