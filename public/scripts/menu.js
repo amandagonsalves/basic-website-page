502876a495aff59396c2b867f75427992202a95e
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-09-15 22:35:19 -0300

    menu

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -6,7 +6,11 @@ const html = {
 }
 html.get('.container__icon-menu').addEventListener('click', e => {
     menu.classList.add('show');
-})
+    html.get('.backdrop-menu').classList.add('show-backdrop')
+});
 html.get('.container__icon-close').addEventListener('click', e => {
     menu.classList.remove('show');
-})
+});
+html.get('.backdrop-menu').addEventListener('click', e => {
+    html.get('.backdrop-menu').classList.remove('show-backdrop')
+});
