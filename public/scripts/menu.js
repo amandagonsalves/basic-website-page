826e5e613649d3ca2e68caa24e416a87a1bd8225
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-09-16 17:15:23 -0300

    menu

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -2,12 +2,6 @@ const menu = document.querySelector('.container__menu');
 
 const icons = document.querySelectorAll('.container__icon-menu');
 
-const html = {
-    get(element) {
-        return document.querySelector(element);
-    }
-};
-
 icons.forEach(icon => {
     icon.addEventListener('click', e => {
         menu.classList.toggle('show');
