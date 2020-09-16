2be295ecab4bd06c3e2d5f222c83204946a80cdd
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-09-16 17:05:50 -0300

    menu

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,16 +1,15 @@
 const menu = document.querySelector('.container__menu');
+
+const icons = document.querySelectorAll('.container__icon-menu');
+
 const html = {
     get(element) {
         return document.querySelector(element);
     }
-}
-html.get('.container__icon-menu').addEventListener('click', e => {
-    menu.classList.add('show');
-    html.get('.backdrop-menu').classList.add('show-backdrop')
-});
-html.get('.container__icon-close').addEventListener('click', e => {
-    menu.classList.remove('show');
-});
-html.get('.backdrop-menu').addEventListener('click', e => {
-    html.get('.backdrop-menu').classList.remove('show-backdrop')
+};
+
+icons.forEach(icon => {
+    icon.addEventListener('click', e => {
+        menu.classList.toggle('show');
+    });
 });
