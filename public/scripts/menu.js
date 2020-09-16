a57c68d98097c6b103511b8e0e4e73c1fd860db9
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-09-16 18:22:23 -0300

    menu

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,9 +1,16 @@
-const menu = document.querySelector('.container__menu');
+const html = {
+    get(element) {
+        return document.querySelector(element);
+    }
+};
 
+const menu = html.get('.container__menu');
+const backdropMenu = html.get('.backdrop-menu');
 const icons = document.querySelectorAll('.container__icon-menu');
 
 icons.forEach(icon => {
     icon.addEventListener('click', e => {
         menu.classList.toggle('show');
+        backdropMenu.classList.toggle('show-backdrop');
     });
 });
