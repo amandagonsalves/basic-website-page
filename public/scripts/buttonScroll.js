8d3b00dce531b18cf5183359e6eb7714af0ad59a
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-09-18 17:28:30 -0300

    scroll

--- public/scripts/buttonScroll.js
+++ public/scripts/buttonScroll.js
@@ -9,30 +9,29 @@ const job = html.get('#job');
 const aboutContent = html.get('#about-content');
 const search = html.get('.section__search'); 
 
+const aboutContentTop = aboutContent.offsetTop;
+const searchTop = search.offsetTop;
+const aboutButtonTop = aboutContentTop * 3.3 / 4;
+const jobButtonTop = searchTop * 2.3 / 4;
+
 
 function buttonScroll() {
-   const aboutContentTop = aboutContent.offsetTop;
-   const searchTop = search.offsetTop;
-   const aboutButtonTop = aboutContentTop * 3.3 / 4;
-   const jobButtonTop = searchTop * 2.3 / 4;
 
-    about.addEventListener('click', e => {
-        e.preventDefault();
+   addEventToButton(about, aboutButtonTop);
 
-        scrollToPosition(aboutButtonTop);
-
-   });
-
-   job.addEventListener('click', e => {
-        e.preventDefault();
-
-        scrollToPosition(jobButtonTop)
-
-   });
+   addEventToButton(job, jobButtonTop);
    
 };
 
 buttonScroll();
+
+function addEventToButton(button, elementTop) {
+    button.addEventListener('click', e => {
+        e.preventDefault();
+
+        scrollToPosition(elementTop);
+   });
+}
 
 function scrollToPosition(elementTop) {
     smoothScrollTo(0, elementTop);
