18e17e4e65f5bb27859d2a0e53a1cf124737ca24
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-09-17 23:21:08 -0300

    scroll

--- public/scripts/work-with-us.js
+++ public/scripts/work-with-us.js
@@ -1,2 +1,29 @@
+const html = {
+    get(element) {
+        return document.querySelector(element);
+    }
+}
 
+const about = html.get('#about');
+const job = html.get('#job');
+const aboutContent = html.get('#about-content');
+const search = html.get('.section__search'); 
+
+
+function animeScroll() {
+   const windowTop = window.pageYOffset + (window.innerHeight * 3 / 4);
+   const aboutTop = about.offsetTop;
+   const jobTop = job.offsetTop;
+   const aboutContentTop = aboutContent.offsetTop - 3/4;
+   const searchfor = search.offsetTop;
+
+   about.addEventListener('click', e => {
+       e.preventDefault();
+       window.scroll(0, aboutContentTop - 160);
+   });
+   
+   
+};
+
+animeScroll();
 
