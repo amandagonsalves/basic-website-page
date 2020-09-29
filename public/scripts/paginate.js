3bce318f85eea8daddad8defa927510b6161bdd8
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-09-29 19:48:45 -0300

    post

--- public/scripts/paginate.js
+++ public/scripts/paginate.js
@@ -67,18 +67,20 @@ function paginate() {
 
             paginatedItems.forEach(list.create);
 
-            for (let index = 0; index < data.length; index++) {
                 
-                let url = new URL(`http://localhost:5500/post-blog.html?id=${index}`);
+                let url = location.href;
                 let search_params = url.searchParams;
-
+                console.log(url)
+/* 
                 if (search_params.has('id')) {
                     const id = search_params.get('id');
-                    console.log(id);
-                    console.log(data[id])
-                }
+                    const cards = await getData()
+                    console.log(cards[id].title)
+                    
+                } else {
+                    console.log('no id')
+                } */
 
-            }
 
 
         }
--- public/scripts/post.js
+++ public/scripts/post.js
@@ -1,3 +1,14 @@
-import { getCard } from '../../src/pages';
+import { getData } from './cardsBlog';
 
-getCard();
+let url = location.href;
+let search_params = url.searchParams;
+console.log(url)
+
+if (search_params.has('id')) {
+    const id = search_params.get('id');
+    const cards = await getData()
+    console.log(cards[id].title)
+    
+} else {
+    console.log('no id')
+}
