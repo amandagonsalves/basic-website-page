ecb82877c437f4cc105ba99b6673fc5c8136bb38
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-09-30 19:11:32 -0300

    post

--- public/scripts/post.js
+++ public/scripts/post.js
@@ -23,33 +23,31 @@ async function cardsContent() {
     console.log(idArray)
     console.log(id)
 
-    if(id < 0 || id > cards.length) {
-        /* document.querySelector('.post-header__title').innerHTML = 'Nenhum post encontrado';
-        document.querySelector('.container-post-page').style.display = 'none'; */
+    if (id < 0 || id > cards.length) {
         window.location = '/blog.html';
     } else {
-        console.log('maior q 0')
-    }
-   /*  const titlePage = cards[id].title;
-    const paragraphPage = cards[id].textContent;
-    const keywordsPage = cards[id].keywords;
-    const imagePage = cards[id].thumbnail;
+        const titlePage = cards[id].title;
+        const paragraphPage = cards[id].textContent;
+        const keywordsPage = cards[id].keywords;
+        const imagePage = cards[id].thumbnail;
 
-   
-    html.get('.post-header__title').forEach(element => {
-        element.innerHTML = titlePage;
-    });
-    html.get('.text-container__para').forEach(element => {
-        element.innerHTML = paragraphPage;
-    });
-    html.get('.content-post-page__keywords').forEach(element => {
-        element.innerHTML = keywordsPage;
-    });
-    html.get('.text-container__title').forEach(element => {
-        element.innerHTML = titlePage;
-    });
-    html.get('.content-post-page__image').forEach(element => {
-        element.innerHTML = imagePage;
-    }); */
+
+        html.get('.post-header__title').forEach(element => {
+            element.innerHTML = titlePage;
+        });
+        html.get('.text-container__para').forEach(element => {
+            element.innerHTML = paragraphPage;
+        });
+        html.get('.content-post-page__keywords').forEach(element => {
+            element.innerHTML = keywordsPage;
+        });
+        html.get('.text-container__title').forEach(element => {
+            element.innerHTML = titlePage;
+        });
+        html.get('.content-post-page__image').forEach(element => {
+            element.innerHTML = imagePage;
+        });
+    }
+
 }
-cardsContent()
+cardsContent();
